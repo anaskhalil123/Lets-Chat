cc3ab8696b61e58b80217e8135c72a11b095fa6c
Anas Khalil <64696058+anaskhalil123@users.noreply.github.com>
2023-06-08 14:24:36 +0300

    adding main.dart

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_chat/common/routes/pages.dart';
+import 'package:firebase_chat/common/services/services.dart';
+import 'package:firebase_chat/common/store/config.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart ';
+import 'common/store/user.dart';
+import 'firebase_options.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Get.putAsync<StorageService>(() => StorageService().init());
+  Get.put<ConfigStore>(ConfigStore());
+  Get.put<UserStore>(UserStore());
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) => GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
