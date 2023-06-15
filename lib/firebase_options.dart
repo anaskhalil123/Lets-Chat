8d00309264693215330f0a18446fb1d923359d4d
Anas Khalil <64696058+anaskhalil123@users.noreply.github.com>
2023-06-15 12:02:31 +0300

    Revert "adding a space between strings"
    This reverts commit 5a8056da608bdc8e10d9bf0e351714eefeaab2cb.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -19,7 +19,6 @@ class DefaultFirebaseOptions {
     if (kIsWeb) {
       throw UnsupportedError(
         'DefaultFirebaseOptions have not been configured for web - '
-        
         'you can reconfigure this by running the FlutterFire CLI again.',
       );
     }
